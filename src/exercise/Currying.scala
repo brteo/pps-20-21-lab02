ba8f2082eacca9f31cf1cba768bc25fde107435d
brteo <51654355+brteo@users.noreply.github.com>
2021-04-08 09:50:58 +0200

    nonCurryingDef semplified

--- src/exercise/Currying.scala
+++ src/exercise/Currying.scala
@@ -6,10 +6,7 @@ object Currying{
 
   val curryingVal : Int => Int => Int => Boolean = x => y => z => x<=y && y<=z
 
-  def nonCurryingDef(x:Int, y:Int, z:Int):Boolean = (x,y,z) match {
-    case (x,y,z) if x<=y && y<=z => true
-    case _ => false
-  }
+  def nonCurryingDef(x:Int, y:Int, z:Int):Boolean = x<=y && y<=z
 
   def curryingDef(x:Int)(y:Int)(z:Int):Boolean = x<=y && y<=z
 }
